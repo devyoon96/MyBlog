ec2e505dda6272865a29201aa8e4aa0f9caecd37
devyoon96 <devyoon96@gmail.com>
2023-06-02 11:39:03 +0900

    #2 feat: 블로그 글 목록 조회 기능 구현. Controller, Service, DTO 구현

--- src/main/java/me/devyoon/myblog/service/BlogService.java
+++ src/main/java/me/devyoon/myblog/service/BlogService.java
@@ -6,6 +6,8 @@ import me.devyoon.myblog.dto.AddArticleRequest;
 import me.devyoon.myblog.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor  // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service        // 빈으로 등록
 public class BlogService {
@@ -14,5 +16,10 @@ public class BlogService {
     // 블로그 글 추가 메서드 입니다.
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // findAll()을 호출해 article 테이블에 저장되어 있는 모든 데이터를 조회한다.
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
