0578a2ac1ac50ffe458ea790f0efa40d0579534a
devyoon96 <devyoon96@gmail.com>
2023-06-02 11:40:31 +0900

    #2 feat: 블로그 글 목록 조회 기능 구현. application.properties 수정, 테스트 데이터 삽입 및 컨트롤러 테스트

--- src/test/java/me/devyoon/myblog/controller/BlogApiControllerTest.java
+++ src/test/java/me/devyoon/myblog/controller/BlogApiControllerTest.java
@@ -19,7 +19,9 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -71,4 +73,31 @@ class BlogApiControllerTest {
         assertThat(articles.get(0).getContent()).isEqualTo(content);
     }
 
+    @DisplayName("findAllArticles : 블로그 글 목록 조회에 성공한다.")
+    @Test
+    public void findAllArticles() throws Exception {
+        //given
+        // 블로그 글을 저장한다
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+
+        blogRepository.save(Article.builder()
+                        .title(title)
+                        .content(content)
+                        .build());
+
+        //when
+        // 목록 조회 API를 호출한다.
+        final ResultActions resultActions = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        //응답 코드가 200 OK이고, 반환받은 값 중에 0번째 요소의 content와 title이 저장된 값과 같은지 확인한다
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].content").value(content))
+                .andExpect(jsonPath("$[0].title").value(title));
+
+    }
 }
