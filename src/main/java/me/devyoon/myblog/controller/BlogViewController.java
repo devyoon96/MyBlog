99682685077482843b4d3e30abaf4f507cb9b5c9
devyoon96 <devyoon96@gmail.com>
2023-06-05 17:08:32 +0900

    #2 feat: 블로그 View 구현하기, 컨트롤러 생성, 삭제 기능 구현, data.sql 구현, Article 엔티티 수정

--- src/main/java/me/devyoon/myblog/controller/BlogViewController.java
+++ src/main/java/me/devyoon/myblog/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.devyoon.myblog.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.devyoon.myblog.domain.Article;
 import me.devyoon.myblog.dto.ArticleListViewResponse;
+import me.devyoon.myblog.dto.ArticleViewResponse;
 import me.devyoon.myblog.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,5 +27,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);  // 블로그 글 리스트 저장
 
         return "articleList"; // articleList.html라는 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
