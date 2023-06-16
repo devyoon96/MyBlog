3b2ac58c63725ebdf1befa87c9c89b7bcd8e400c
devyoon96 <devyoon96@gmail.com>
2023-06-16 18:28:09 +0900

    #9 feat: 시큐리티 서비스, 레포지토리 생성 및 수정

---
+++ src/main/java/me/devyoon/myblog/service/UserDetailService.java
@@ -0,0 +1,22 @@
+package me.devyoon.myblog.service;
+
+import lombok.RequiredArgsConstructor;
+import me.devyoon.myblog.domain.User;
+import me.devyoon.myblog.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+// 스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스입니다.
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    // 사용자 이름(email)으로 사용자의 정보를 가져오는 메서드
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+
+}
